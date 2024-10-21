2f0825a06f4b9842f6267c65a74adb5565ec18e1
Diana <diana09877@gmail.com>
2024-10-21 19:59:13 +0600

    first commit

---
+++ toyboss_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+
+class Product(models.Model):
+    category = models.ForeignKey(on_delete=models.CASCADE, verbose_name='categories')
+    name = models.CharField(max_length=255, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    recipes = models.TextField(verbose_name='рецепты')
+    image = models.ImageField()
+
+
